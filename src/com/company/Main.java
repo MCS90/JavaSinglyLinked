c8bf7bfedf018955a0ce6c09d25097bb1b0379e0
mscob <mscobee90@gmail.com>
2021-05-17 02:57:23 -0400

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        MyLinkedList<Integer> mylist = new MyLinkedList<>();
+        mylist.appendNode(5);
+        mylist.appendNode(6);
+        mylist.appendNode(23);
+        mylist.appendNode(104);
+        mylist.appendNode(107);
+        mylist.prepend(10);
+        mylist.print();
+    }
+}
---
+++ src/com/company/MyLinkedList.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class MyLinkedList<T> {
+    private Node head, tail;
+
+    public void appendNode(T data) {
+        if(head == null) {
+            head = new Node(data);
+            return;
+        }
+        tail = head;
+        while(tail.next != null){
+            tail = tail.next;
+        }
+        tail.next = new Node(data);
+
+    }
+    public void prepend(T data) {
+        // 5 > 3 > 9 want to prepend 10
+        Node prevHead = head;
+        head = new Node(data);
+        head.next = prevHead;
+//        System.out.println("head: " + head.data);
+//        System.out.println("prev:" + prevHead.data);
+    }
+
+    public void print() {
+        Node n;
+        n = head;
+        while(n.next != null){
+            System.out.println(n.data);
+            n=n.next;
+        }
+    }
+
+    class Node<T> {
+        private T data;
+        private Node next;
+        public Node(T data){
+            this.data = data;
+        }
+    }
+}
